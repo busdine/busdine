84788f8c6125b6574bae1a1ee5440c9c5b55c365
shvmgrx <s.goraksha@jacobs-university.de>
2020-12-10 19:34:16 +0100

    header and contact page done

--- bck/assets/js/contact.js
+++ bck/assets/js/contact.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
     
     jQuery.validator.addMethod('answercheck', function (value, element) {
         return this.optional(element) || /^\bcat\b$/.test(value)
-    }, "type the correct answer -_-");
+    }, "type the correct answer");
 
     // validate contactForm form
     $(function() {
@@ -32,52 +32,57 @@ $(document).ready(function(){
                     required: true,
                     minlength: 20
                 }
+                privacyCheck: {
+                    required: true,
+                    
+                }
             },
             messages: {
                 name: {
-                    required: "come on, you have a name, don't you?",
-                    minlength: "your name must consist of at least 2 characters"
+                    required: "Please enter your name. ",
+                    minlength: "Your name must consist of atleast 2 characters."
                 },
                 subject: {
-                    required: "come on, you have a subject, don't you?",
-                    minlength: "your subject must consist of at least 4 characters"
+                    required: "Please add subject",
+                    minlength: "The subject must consist of atleast 4 characters."
                 },
                 number: {
-                    required: "come on, you have a number, don't you?",
-                    minlength: "your Number must consist of at least 5 characters"
+                    required: "Please add your phone number so we could get back to you.",
+                    minlength: "The number must consist of atleast 4 characters."
                 },
                 email: {
-                    required: "no email, no message"
+                    required: "Please enter your email."
                 },
                 message: {
-                    required: "um...yea, you have to write something to send this form.",
-                    minlength: "thats all? really?"
+                    required: "Please enter the message.",
+                    
                 }
+
             },
-            submitHandler: function(form) {
-                $(form).ajaxSubmit({
-                    type:"POST",
-                    data: $(form).serialize(),
-                    url:"contact_process.php",
-                    success: function() {
-                        $('#contactForm :input').attr('disabled', 'disabled');
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $(this).find(':input').attr('disabled', 'disabled');
-                            $(this).find('label').css('cursor','default');
-                            $('#success').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#success').modal('show');
-                        })
-                    },
-                    error: function() {
-                        $('#contactForm').fadeTo( "slow", 1, function() {
-                            $('#error').fadeIn()
-                            $('.modal').modal('hide');
-		                	$('#error').modal('show');
-                        })
-                    }
-                })
-            }
+            // submitHandler: function(form) {
+            //     $(form).ajaxSubmit({
+            //         type:"POST",
+            //         data: $(form).serialize(),
+            //         url:"contact_process.php",
+            //         success: function() {
+            //             $('#contactForm :input').attr('disabled', 'disabled');
+            //             $('#contactForm').fadeTo( "slow", 1, function() {
+            //                 $(this).find(':input').attr('disabled', 'disabled');
+            //                 $(this).find('label').css('cursor','default');
+            //                 $('#success').fadeIn()
+            //                 $('.modal').modal('hide');
+		    //             	$('#success').modal('show');
+            //             })
+            //         },
+            //         error: function() {
+            //             $('#contactForm').fadeTo( "slow", 1, function() {
+            //                 $('#error').fadeIn()
+            //                 $('.modal').modal('hide');
+		    //             	$('#error').modal('show');
+            //             })
+            //         }
+            //     })
+            // }
         })
     })
         
